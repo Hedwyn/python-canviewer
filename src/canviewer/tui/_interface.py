8570fe4ee1fcc952cf9eeef960a3dcd8bde97149
Baptiste Pestourie <bpestourie@gmail.com>
2025-12-19 12:05:57 +0100

    move TUI entrypoint to dedicated __main__.py

--- src/canviewer/tui/_interface.py
+++ src/canviewer/tui/_interface.py
@@ -699,19 +699,3 @@ class CanViewer(App[None]):
             assert isinstance(widget, SignalWidget)
             is_tx = new_producer in properties.senders
             widget.is_tx = is_tx
-
-
-if __name__ == "__main__":
-    import json
-    import sys
-
-    import can
-
-    logging.basicConfig(filename="tui.log", level=logging.INFO)
-    store = DatabaseStore.from_files(*sys.argv[1:])
-    backend = Backend(store, channel="vcan0", interface="socketcan")
-    dispatcher = WidgetDispatcher(store)
-    with open("widgets.json", "w+") as f:
-        f.write(json.dumps((dispatcher.serialize_model()), indent=4, default=str))
-    viewer = CanViewer(backend=backend)
-    viewer.run()
